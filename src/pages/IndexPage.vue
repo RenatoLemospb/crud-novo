1a04e2f4b06fe63e6b477f085773c36e7ef5e7ed
RenatoLemospb <renatolemosmendes@gmail.com>
2023-06-04 21:42:49 -0300

    login ao centro

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,41 +1,41 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page-container align="center" class>
     <body>
-    <div class="container">
-        <h1>
+      <div class="container">
+         <h1>
             Login
-        </h1>
-        <form method="post" action="">
-            <div>
-                <label for="user">
-                  User
-                </label>
-                <input type="user" name="user" placeholder="Digite seu Login">
+         </h1>
+          <form method="post" action="">
+              <div>
+                  <label for="user">
+                    User
+                  </label>
+                  <input type="user" name="user" placeholder="Digite seu Login">
 
-            </div>
-            <div>
-                <label for="password">
-                   Senha
-                </label>
-                <input type="password" name="password" placeholder="Digite sua senha">
-            </div>
-            <div>
-                <button type="submit">
-                    Enviar
-                </button>
+              </div>
+              <div>
+                  <label for="password">
+                    Senha
+                  </label>
+                  <input type="password" name="password" placeholder="Digite sua senha">
+              </div>
+               <div>
+                  <button type="submit">
+                      Enviar
+                  </button>
 
-            </div>
-        </form>
-    </div>
+              </div>
+          </form>
+      </div>
 
-</body>
+    </body>
 
     <!-- <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     > -->
-  </q-page>
+  </q-page-container>
 </template>
 
 <script>
