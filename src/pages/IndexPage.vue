4734a87363967c5c3aac8a8fa9c9a40f9ca836f6
RenatoLemospb <renatolemosmendes@gmail.com>
2023-06-04 22:58:57 -0300

    label class no user e senha

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,32 +2,28 @@
   <q-page-container align="center" class>
     <body>
       <div class="container">
-         <h1>
-            Login
-         </h1>
-          <form method="post" action="">
-              <div>
-                  <label for="user">
-                    User
-                  </label>
-                  <input type="user" name="user" placeholder="Digite seu Login">
-
-              </div>
-              <div>
-                  <label for="password">
-                    Senha
-                  </label>
-                  <input type="password" name="password" placeholder="Digite sua senha">
-              </div>
-               <div>
-                  <button type="submit">
-                      Enviar
-                  </button>
-
-              </div>
-          </form>
+        <h1>Login</h1>
+        <form method="post" action="">
+          <div>
+            <label class="user" for>User </label>
+            <input
+             type="user"
+             name="user"
+             placeholder="Digite seu Login"/>
+          </div>
+          <div>
+            <label class="password" for>Senha </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Digite sua senha"
+            />
+          </div>
+          <div>
+            <button type="submit">Enviar</button>
+          </div>
+        </form>
       </div>
-
     </body>
 
     <!-- <img
